af14b0bd846a3fb1084168b0b5a3f61adfbaa97f
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-01-10 23:45:29 +0900

    Add hero section html and css styles

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// const siteNavLink = document.querySelectorAll('.sitenav__link');
+
+// siteNavLink.forEach((item) => {
+//   item.addEventListener('click', ()=> {
+//     item.classList.toggle('sitenav__link-active')
+
+//   })
+// })
