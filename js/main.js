1ee98476393110709b174f942a115a3f683a82fd
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-01-15 20:11:12 +0900

    Add sitenav link activete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-// const siteNavLink = document.querySelectorAll('.sitenav__link');
+const siteNavLink = document.querySelectorAll('.sitenav__link');
+const siteNavItem = document.querySelectorAll('.sitenav__item');
 
-// siteNavLink.forEach((item) => {
-//   item.addEventListener('click', ()=> {
-//     item.classList.toggle('sitenav__link-active')
 
-//   })
-// })
+function deactiveItems () {
+  siteNavItem.forEach((item) => {
+    item.classList.remove('sitenav__item-active');
+
+  });
+
+  siteNavLink.forEach((links) => {
+    links.classList.remove('sitenav__link-active');
+  });
+};
+
+siteNavLink.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    deactiveItems();
+    link.parentElement.classList.add('sitenav__item-active');
+    link.classList.add('sitenav__link-active');
+  });
+});
