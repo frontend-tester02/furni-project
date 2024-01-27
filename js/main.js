df1c64b633c441c3e79138b1b851b5688902c445
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-01-27 22:10:10 +0900

    Add active testimonials

--- js/main.js
+++ js/main.js
@@ -23,40 +23,21 @@ siteNavLink.forEach((link) => {
 });
 
 
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper('.swiper', {
+  autoHeight: true,
   slidesPerView: 1,
-  spaceBetween: 30,
+  grabCursor: true,
   loop: true,
+
+  // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
+
+  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
-
-// // TESTIMONIALS
-
-// const tsnItem = document.querySelector('.testimonials__item')
-// const tsnActive = document.querySelector('.testimonials__content');
-
-// // prev and next
-// const tsnNext = document.querySelector('.testimonials__control__next');
-// const tsnPrev = document.querySelector('.testimonials__control__prev');
-
-// function activeTsn () {
-//   tsnNext.addEventListener('click', () => {
-//     tsnItem.innerHTML = `
-//     <div class="testimonials__content>
-//     <p class="testimonials__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, eos ullam. Molestiae magni dolore quidem illum assumenda? Explicabo dolores provident iste! Soluta neque voluptas dolorem deleniti animi exercitationem reiciendis? Aperiam, illum. Officiis voluptas voluptatum, tempore iure quibusdam corrupti labore fugiat impedit obcaecati tempora repellendus excepturi minus. Tempora facilis eum ipsa?</p>
-//     <img class="testimonials__img" src="img/person-1.png" alt="Testimonials person image" width="80" height="77">
-//     <h3 class="testimonials__name">Maria Jones</h3>
-//     <p class="testimonials__job-name">CEO, Co-Founder, XYZ Inc.</p>
-//     </div>
-//     `
-//   })
-// };
-
-// activeTsn()
