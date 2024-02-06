7c2a2854e3fb56b687260ae4f1eb0383f3c5269c
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-02-06 22:40:20 +0900

    Add shopping part of project

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 
 // Site header links
-const siteNavLink = document.querySelectorAll('.sitenav__link');
-const siteNavItem = document.querySelectorAll('.sitenav__item');
+// const siteNavLink = document.querySelectorAll('.sitenav__link');
+// const siteNavItem = document.querySelectorAll('.sitenav__item');
 
 
-function deactiveItems () {
-  siteNavItem.forEach((item) => {
-    item.classList.remove('sitenav__item-active');
+// function deactiveItems () {
+//   siteNavItem.forEach((item) => {
+//     item.classList.remove('sitenav__item-active');
 
-  });
+//   });
 
-  siteNavLink.forEach((links) => {
-    links.classList.remove('sitenav__link-active');
-  });
-};
+//   siteNavLink.forEach((links) => {
+//     links.classList.remove('sitenav__link-active');
+//   });
+// };
 
-siteNavLink.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    deactiveItems();
-    link.parentElement.classList.add('sitenav__item-active');
-    link.classList.add('sitenav__link-active');
-  });
-});
+// siteNavLink.forEach((link) => {
+//   link.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     deactiveItems();
+//     link.parentElement.classList.add('sitenav__item-active');
+//     link.classList.add('sitenav__link-active');
+//   });
+// });
 
 
 // Testimonials
