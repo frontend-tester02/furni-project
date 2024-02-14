0a375e3cf1968c94f182b2f77d271c0523e5ca08
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2024-02-14 23:25:45 +0900

    Updated sitenav links javascript part

--- js/main.js
+++ js/main.js
@@ -1,28 +1,4 @@
-
-// Site header links
-// const siteNavLink = document.querySelectorAll('.sitenav__link');
-// const siteNavItem = document.querySelectorAll('.sitenav__item');
-
-
-// function deactiveItems () {
-//   siteNavItem.forEach((item) => {
-//     item.classList.remove('sitenav__item-active');
-
-//   });
-
-//   siteNavLink.forEach((links) => {
-//     links.classList.remove('sitenav__link-active');
-//   });
-// };
-
-// siteNavLink.forEach((link) => {
-//   link.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     deactiveItems();
-//     link.parentElement.classList.add('sitenav__item-active');
-//     link.classList.add('sitenav__link-active');
-//   });
-// });
+  ready()
 
 
 // Testimonials
